786f95bce2a77347b112088d2899032fffea752e
RFD <reborn.from.deadlock@gmail.com>
2023-11-13 23:53:39 +0800

    刪掉 HTTPS SERVER

--- site/python-https-server/run_server.bat
+++
@@ -1,8 +0,0 @@
-REM set PYTHON_DIR=*
-REM OpenSSL fix for Anacodna
-REM SET PATH=%PATH%;%PYTHON_DIR%\Library\bin\
-REM %PYTHON_DIR%\python.exe" https_server\file_server.py
-
-python.exe https_server\file_server.py
-
-set /p DUMMY=Hit ENTER to continue...
